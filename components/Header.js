1674543d076478b695b7f548bd162bf72ec2bb18
cristianbgp <cristian.granda.pastor@gmail.com>
2020-11-13 11:12:45 -0500

    Add social icons

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import styles from "../styles/Header.module.css";
+import Instagram from "../icons/Instagram";
+import Twitter from "../icons/Twitter";
 
 export default function Header() {
   return (
@@ -10,6 +12,29 @@ export default function Header() {
         <p className={styles.description}>
           Una historia sobre los protagonistas del golpe.
         </p>
+        <div className={styles.socialIcons}>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://www.instagram.com/golpistas.pe/"
+          >
+            <div className={styles.iconContainer}>
+              <Instagram />
+            </div>
+            <span>golpistas.pe</span>
+          </a>
+          <span className={styles.separator}>|</span>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://twitter.com/golpistasPeru"
+          >
+            <div className={styles.iconContainer}>
+              <Twitter />
+            </div>
+            <span>golpistasPeru</span>
+          </a>
+        </div>
       </div>
     </div>
   );
